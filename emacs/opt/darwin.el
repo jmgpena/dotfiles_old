968f847f6af2b02d66b7d42b6d9280a0f3f9313b
jmgpena <jorge@jmgpena.net>
2013-06-12 09:31:46 +0100

    Darwin specific changes

---
+++ emacs/opt/darwin.el
@@ -0,0 +1,8 @@
+;; osx specific config
+(push "/usr/local/bin" exec-path)
+(push "usr/texbin" exec-path)
+(setq mac-command-modifier 'super)
+(setq mac-option-modifier 'meta)
+(setq mac-right-option-modifier 'none)
+(setq ns-function-modifier 'hyper)
+(setq mac-allow-anti-aliasing t)
