2627f3ca679163b8d1f8342c3e1ce415f761d94d
jmgpena <jorge@jmgpena.net>
2013-06-19 17:11:37 +0100

    fix merge conflict

--- emacs/init.el
+++ emacs/init.el
@@ -137,6 +137,12 @@ By Xah Lee"
                :pkgname "jmgpena/lilypond-mode"
                :prepare (load "lilypond-init"))
         (:name web-mode)
+        (:name jade-mode
+               :type github
+               :pkgname "brianc/jade-mode")
+        (:name handlebars-mode
+               :type github
+               :pkgname "danielevans/handlebars-mode")
         (:name solarized-theme
                :type github
                :pkgname "sellout/emacs-color-theme-solarized"
