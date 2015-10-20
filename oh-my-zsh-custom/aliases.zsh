65e420621446fc0c51fc68fb097f190ef316fcbb
jmgpena <jorge@pond5.com>
2015-10-20 14:53:26 +0200

    Add some new aliases to zsh

--- oh-my-zsh-custom/aliases.zsh
+++ oh-my-zsh-custom/aliases.zsh
@@ -1,2 +1,6 @@
-alias me='emacs -nw -Q'
-alias sme='sudo emacs -nw -Q'
+alias me="emacs -nw"
+alias sme="sudo emacs -nw"
+
+alias vbm="VBoxManage"
+alias p5-vm-start="VBoxManage startvm pond5-single.dev --type headless"
+alias p5-vm-stop="VBoxManage controlvm pond5-single.dev acpipowerbutton"
