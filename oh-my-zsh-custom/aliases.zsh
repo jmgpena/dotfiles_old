50ba5e0ea3d71af6c6bf55d668b38f432df882c3
jmgpena <jmgpena@gmail.com>
2019-01-08 15:56:26 +0000

    Remove zsh stuff

--- oh-my-zsh-custom/aliases.zsh
+++
@@ -1,6 +0,0 @@
-alias me="emacs -nw"
-alias sme="sudo emacs -nw"
-
-alias vbm="VBoxManage"
-alias p5-vm-start="VBoxManage startvm pond5-single.dev --type headless"
-alias p5-vm-stop="VBoxManage controlvm pond5-single.dev acpipowerbutton"
